09c471622bb38bfb9a93b239865d7f97c6407271
Danillo Martins <131684928+DanilloMartiins@users.noreply.github.com>
2026-02-11 16:35:26 -0300

    feat: adiciona verificador de número primo em Python

---
+++ python/desafio01_primo.py
@@ -0,0 +1,10 @@
+def eh_primo(numero: int) -> bool:
+    if numero <= 1:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    print("7 é primo?", eh_primo(7))
